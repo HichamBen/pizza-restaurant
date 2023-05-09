e7103e89890d234c833a46bf7985cd382ee3c364
HichamBen <hbl73801@gmail.com>
2023-05-09 17:40:21 +0100

    Fix issue with res

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -6,12 +6,15 @@ window.onload = () => {
 
   burgerMenu.addEventListener("click", () => {
     linksMenu.classList.remove("translate-x-full");
+    linksMenu.classList.remove("invisible");
+
     closeMenu.classList.remove("hidden");
     burgerMenu.classList.add("hidden");
   });
 
   closeMenu.addEventListener("click", () => {
     linksMenu.classList.add("translate-x-full");
+    linksMenu.classList.add("invisible");
     burgerMenu.classList.remove("hidden");
     closeMenu.classList.add("hidden");
   });
