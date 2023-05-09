e02b9224f776465b08899732e26b0a474b4bb9af
HichamBen <hbl73801@gmail.com>
2023-05-09 15:54:38 +0100

    finishing first version

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -15,39 +15,4 @@ window.onload = () => {
     burgerMenu.classList.remove("hidden");
     closeMenu.classList.add("hidden");
   });
-
-  // carousel
-  let carousel = document.getElementById("carousel");
-  let carouselBtns = document.getElementById("carousel-btn");
-  let itemId = 0;
-
-  let timer = setInterval(() => {
-    animateCarousel();
-  }, 4000);
-
-  for (let i = 0; i < carouselBtns.children.length; i++) {
-    carouselBtns.children[i].addEventListener("click", () => {
-      clearInterval(timer);
-      itemId = i - 1;
-      animateCarousel();
-      timer = setInterval(() => {
-        animateCarousel();
-      }, 4000);
-    });
-  }
-
-  function animateCarousel() {
-    itemId = itemId + 1 === 3 ? 0 : itemId + 1;
-    for (let i = 0; i < carousel.children.length; i++) {
-      if (i === itemId) {
-        carousel.children[i].classList.remove("opacity-0", "invisible");
-        carousel.children[i].classList.add("opacity-100");
-        carouselBtns.children[i].classList.add("bg-white");
-      } else {
-        carousel.children[i].classList.remove("opacity-100");
-        carousel.children[i].classList.add("opacity-0", "invisible");
-        carouselBtns.children[i].classList.remove("bg-white");
-      }
-    }
-  }
 };
